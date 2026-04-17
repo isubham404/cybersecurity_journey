818c1b4b11dd42e0693a095888db5d35c87cd52a
isubham404 <subhamkar725@gmail.com>
2026-04-17 22:39:44 +0530

    Day 8 task done

---
+++ Day8/multi_user_auth.py
@@ -0,0 +1,18 @@
+import hashlib 
+users = {"subham": hashlib.sha256("Subham@123".encode()).hexdigest()
+         ,"admin": hashlib.sha256("admin@123".encode()).hexdigest()
+         ,"sarthak" : hashlib.sha256("sarThak12@2006".encode()).hexdigest()
+         }
+
+userName = input("Enter the user name : ")
+passWord = input("Enter your password : ")
+
+if userName in users :
+    hashed_input =  hashlib.sha256(passWord.encode()).hexdigest()
+    
+    if users[userName] == hashed_input:
+        print("Login Successfull !!")
+    else:
+        print("Incorrect Password !! ")
+else :
+    print("User Not found !! ")
